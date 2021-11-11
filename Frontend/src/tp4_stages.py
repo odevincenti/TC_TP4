2172908b9ac0b9a40a6503573b92a2cf056d2591
valen <vvenier@itba.edu.ar>
2021-11-11 01:01:45 -0300

    micho k-po19: Despues todo stages

--- Frontend/src/tp4_stages.py
+++ Frontend/src/tp4_stages.py
@@ -56,11 +56,25 @@ class Stages (QWidget, Ui_Form):
     def create_stage (self):
         print("create stage")
         self.aux_stage = StageWidget()
+        self.aux_stage.label.setText("Stage " + str(len(self.stage_array) + 1))
         self.stage_array.append(self.aux_stage)
         self.Stages_Widget.layout().addWidget(self.aux_stage)
 
     def delete_stage(self):
-        print("delete stage")
+        i = 0
+        j = 0
+        while i < len(self.stage_array):
+            if self.stage_array[i].radioButton.isChecked() == 0:
+                print("caca")
+            else:
+                self.stage_array[i].hide()
+                self.stage_array.pop(i)
+                print("delete stage")
+                i = i - 1
+            i = i + 1
+        while j != len(self.stage_array):
+            self.stage_array[j].label.setText("Stage " + str(j + 1))
+            j = j + 1
 
     def selected (self):
         print("selected")
--- Frontend/src/ui/tp4_stages.py
+++ Frontend/src/ui/tp4_stages.py
@@ -42,7 +42,7 @@ class Ui_Form(object):
         self.verticalLayout_6.setObjectName("verticalLayout_6")
         self.horizontalLayout_14 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_14.setObjectName("horizontalLayout_14")
-        self.PyZ_Widget = QtWidgets.QWidget(Form)
+        self.PyZ_Widget = MplWidget(Form)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -375,7 +375,7 @@ class Ui_Form(object):
         self.label_28.setText(_translate("Form", "Hz"))
         self.label_32.setText(_translate("Form", "Stage 1"))
         self.delete_stage_button.setText(_translate("Form", "DELETE"))
-from mplwidget import MplWidget
+from Frontend.src.mplwidget import MplWidget
 
 
 if __name__ == "__main__":
